373db8f5db41294778ee1dd00a2618e6852696eb
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2020-06-06 00:25:13 +0100

    Update drag_and_drop.py

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -1,10 +1,7 @@
-import os
 import sys
 import subprocess
 
 
-script_path = os.path.join(os.path.dirname(__file__), 'zs-dl.py')
-call = ['python', script_path, '-u']
-for f in sys.argv[1:]:
-	call.append(f)
+call = ['python', 'zs-dl.py', '-u']
+call.extend(sys.argv[1:])
 subprocess.Popen(call)
